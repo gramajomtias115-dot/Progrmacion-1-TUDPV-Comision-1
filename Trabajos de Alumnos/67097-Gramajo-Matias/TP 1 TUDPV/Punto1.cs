cc2e4f22ab1032f2f995e428c762a08a7d7f53a6
gramajomtias115-dot <gramajomtias115@gmail.com>
2026-09-23 17:59:53 -0300

    Punto 1

---
+++ Trabajos de Alumnos/67097-Gramajo-Matias/TP 1 TUDPV/Punto1.cs
@@ -0,0 +1,34 @@
+﻿string[] armas = {"Espada", "Hacha", "Arco", "Daga"};
+
+    int[] durabilidad = new int [armas.Length];
+
+for (int i = 0; i < armas.Length; i++)
+{
+    Console.WriteLine("Ingrese la durabilidad de " + armas[i] + ":");
+    durabilidad[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("1: Inspeccionar armas críticas");
+Console.WriteLine("2: Ver arsenal completo");
+int eleccion = int.Parse(Console.ReadLine());
+
+switch (eleccion)
+{
+    case 1:
+        for (int i = 0; i < armas.Length; i++)
+        {
+            if (durabilidad[i] <= 20)
+            {
+                Console.WriteLine(armas[i] + "EN ESTADO CRITICO");
+            }
+        }
+
+        break;
+    case 2:
+        for (int i = 0; i < armas.Length; i++)
+        {
+            Console.WriteLine(armas[i]);
+        }
+        break;
+
+}
