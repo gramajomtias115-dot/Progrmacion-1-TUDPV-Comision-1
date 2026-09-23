cf4f181e47403e77fc57ef0ae3c38bedcd107a5f
gramajomtias115-dot <gramajomtias115@gmail.com>
2026-09-23 19:01:52 -0300

    Punto 2

---
+++ Trabajos de Alumnos/67097-Gramajo-Matias/Punto2.cs
@@ -0,0 +1,35 @@
+float[] tiempos = new float[4];
+float[] objetivos = new float [2];
+bool menu = true;
+for (int i = 0; i < tiempos.Length; i++)
+{
+    Console.WriteLine("Ingrese tiempo del corredor " + (i+1));
+    tiempos[i] = float.Parse(Console.ReadLine());  
+
+}
+
+while (menu)
+{
+    Console.WriteLine("Definir tiempo obejtivo 1");
+    objetivos[0] = float.Parse(Console.ReadLine());  
+
+    Console.WriteLine("Definir tiempo obejtivo 2");
+    objetivos[1] = float.Parse(Console.ReadLine());  
+
+    menu = false;
+
+}
+
+for (int i = 0; i < tiempos.Length; i++)
+{
+    if(tiempos[i] <= objetivos[0])
+    {
+        Console.WriteLine("El juegador " + (i+1) + " Supero el primer objetivo");
+        
+    }
+    if(tiempos[i] <= objetivos[1])
+    {
+            Console.WriteLine("El juegador " + (i+1) + " Supero el segundo objetivo");
+
+    }
+}
